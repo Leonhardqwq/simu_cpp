114e615b921f027f197bf03529f3e68d2d09cd23
qiaoli05 <qiaoli05@meituan.com>
2026-01-16 11:22:44 +0800

    版本测试 	modified:   inc/calculate_position_new.hpp

--- inc/calculate_position_new.hpp
+++ inc/calculate_position_new.hpp
@@ -93,15 +93,17 @@ public:
             auto x_each = z.anim[static_cast<unsigned int>(floored_current_frame) - z.begin_frame];            
         */
         float dx = z.anim[static_cast<unsigned int>(reanim.progress*(z.n_frames-1))];
+        /*
         if (state.is_frozen()) return 0.0f;
         if (state.is_slowed()) return dx * 0.01f * (reanim.fps * 0.5f);
         return dx * 0.01f * reanim.fps;
-        /* 老版本逻辑交换律
+        //*/
+        ///* 老版本逻辑交换律
         if (state.is_frozen()) return 0.0f;
         if (state.is_slowed()) dx *= 0.01f * (reanim.fps * 0.5f);
         else                   dx *= 0.01f * reanim.fps;
         return dx;
-        */
+        //*/
     }
     // 常量位移
     static float get_dx_constant(float v0, const CdState& state) {
